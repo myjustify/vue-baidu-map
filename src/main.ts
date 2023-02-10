2084794b092ad3d8f6fe61754fceec60061c5615
duanyong <duanyong@mysteelsoft.com.cn>
2023-02-10 16:14:39 +0800

    隐藏ak

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import App from './App.vue'
 import BaiduMap from "./components"
 const app = createApp(App)
 app.use(BaiduMap, {
-    ak: 'fOT3G6uPsE5acPKkiG9zWiOmPpHl8w5L'
+    ak: import.meta.env.VITE_BAIDUMAP_AK
 })
 app.mount('#app')
